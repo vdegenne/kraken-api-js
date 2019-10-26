5966be780558ab1cbb8d4a0e0647c947ee574d93
Valentin Degenne <vdegenne@gmail.com>
2019-10-26 22:28:31 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# kraken-js
+
+JavaScript full support of [`kraken-api`](https://github.com/nothingisdead/npm-kraken-api).
+Originally `kraken-api` is **CommonJS** friendly only. This package is a port for **CommonJS**, **ES2015 Module** and **TypeScript** support.
+
+## Imports
+
+### CommonJS
+
+```javascript
+const KrakenClient = require('kraken-js')
+```
+
+### ES-Module
+
+```javascript
+import { KrakenClient } from 'kraken-js'
+```
+
+### TypeScript (typings included)
+
+```javascript
+import { KrakenClient } from 'kraken-js'
+```
+
+## Usage
+
+```javascript
+const key = '' // your API key
+const privateKey = '' // your API private key
+const kraken = new KrakenClient(key, privateKey)
+
+// example
+const main = async () => {
+  /* public endpoint */
+  const json = await kraken.api('Ticker', { pair: 'XRPUSD' })
+  console.log(json.result.XXRPZUSC.c) // print current value of XRP
+
+  /* private endpoint */
+  const json = await kraken.api('Balance')
+  console.log(json.result.XXBT) // print how much Bitcoin you have on your account right now
+}
+
+main()
+```
+
+## public methods
+
+`Time` `Assets` `AssetPairs` `Ticker` `Depth` `Trades` `Spread` `OHLC`
+
+## private methods
+
+`Balance` `TradeBalance` `OpenOrders` `ClosedOrders` `QueryOrders` `TradesHistory` `QueryTrades` `OpenPositions` `Ledgers` `QueryLedgers` `TradeVolume` `AddOrder` `CancelOrder` `DepositMethods` `DepositAddresses` `DepositStatus` `WithdrawInfo` `Withdraw` `WithdrawStatus` `WithdrawCancel`
+
+## Contact
+
+vdegenne (at) gmail (dot) com
+
+## support
+
+BTC donation address : 3KfrLBhWgKxnNH9hoC8ipSPYi3LW3Qrj8i
+ETH donation address : 0x2b168Ec72b8B6168E3A0BAFa3A2f356374C880C5
---
+++ _kraken.ts
@@ -0,0 +1,175 @@
+import got from 'got'
+import {createHash,createHmac} from 'crypto'
+import * as qs from 'qs'
+
+// Public/Private method names
+const methods = {
+  public: ['Time', 'Assets', 'AssetPairs', 'Ticker', 'Depth', 'Trades', 'Spread', 'OHLC'],
+  private: ['Balance', 'TradeBalance', 'OpenOrders', 'ClosedOrders', 'QueryOrders', 'TradesHistory', 'QueryTrades', 'OpenPositions', 'Ledgers', 'QueryLedgers', 'TradeVolume', 'AddOrder', 'CancelOrder', 'DepositMethods', 'DepositAddresses', 'DepositStatus', 'WithdrawInfo', 'Withdraw', 'WithdrawStatus', 'WithdrawCancel']
+}
+
+// Default options
+const defaults = {
+  url: 'https://api.kraken.com',
+  version: 0,
+  timeout: 5000
+}
+
+// Create a signature for a request
+const getMessageSignature = (path:string, request:any, secret:string, nonce:string) => {
+  const message = qs.stringify(request)
+  const secret_buffer = Buffer.from(secret, 'base64')
+  const hash = createHash('sha256')
+  const hmac = createHmac('sha512', secret_buffer)
+  // @ts-ignore
+  const hash_digest = hash.update(nonce + message).digest('binary')
+  // @ts-ignore
+  const hmac_digest = hmac.update(path + hash_digest, 'binary').digest('base64')
+
+  return hmac_digest
+}
+
+// Send an API request
+const rawRequest = async (url:string, headers:any, data:any, timeout:number) => {
+  // Set custom User-Agent string
+  headers['User-Agent'] = 'Kraken Javascript API Client'
+
+  const options = { headers, timeout }
+
+  Object.assign(options, {
+    method: 'POST',
+    body: qs.stringify(data)
+  })
+
+  const { body } = await got(url, options)
+  const response = JSON.parse(body)
+
+  if (response.error && response.error.length) {
+    const error = response.error.filter((e:string) => e.startsWith('E')).map((e:string) => e.substr(1))
+
+    if (!error.length) {
+      throw new Error('Kraken API returned an unknown error')
+    }
+
+    throw new Error(error.join(', '))
+  }
+
+  return response
+}
+
+/**
+ * KrakenClient connects to the Kraken.com API
+ * @param {String}        key               API Key
+ * @param {String}        secret            API Secret
+ * @param {String|Object} [options={}]      Additional options. If a string is passed, will default to just setting `options.otp`.
+ * @param {String}        [options.otp]     Two-factor password (optional) (also, doesn't work)
+ * @param {Number}        [options.timeout] Maximum timeout (in milliseconds) for all API-calls (passed to `request`)
+ */
+class KrakenClient {
+
+  protected config: any
+
+  constructor(key:string, secret:string, options:any) {
+    // Allow passing the OTP as the third argument for backwards compatibility
+    if (typeof options === 'string') {
+      options = { otp: options }
+    }
+
+    this.config = Object.assign({ key, secret }, defaults, options)
+  }
+
+  /**
+	 * This method makes a public or private API request.
+	 * @param  {String}   method   The API method (public or private)
+	 * @param  {Object}   params   Arguments to pass to the api call
+	 * @param  {Function} callback A callback function to be executed when the request is complete
+	 * @return {Object}            The request object
+	 */
+  api(method:string, params:any, callback:Function) {
+    // Default params to empty object
+    if (typeof params === 'function') {
+      callback = params
+      params = {}
+    }
+
+    if (methods.public.includes(method)) {
+      return this.publicMethod(method, params, callback)
+    } else if (methods.private.includes(method)) {
+      return this.privateMethod(method, params, callback)
+    } else {
+      throw new Error(method + ' is not a valid API method.')
+    }
+  }
+
+  /**
+	 * This method makes a public API request.
+	 * @param  {String}   method   The API method (public or private)
+	 * @param  {Object}   params   Arguments to pass to the api call
+	 * @param  {Function} callback A callback function to be executed when the request is complete
+	 * @return {Object}            The request object
+	 */
+  publicMethod(method:string, params:any, callback:Function) {
+    params = params || {}
+
+    // Default params to empty object
+    if (typeof params === 'function') {
+      callback = params
+      params = {}
+    }
+
+    const path = '/' + this.config.version + '/public/' + method
+    const url = this.config.url + path
+    const response = rawRequest(url, {}, params, this.config.timeout)
+
+    if (typeof callback === 'function') {
+      response.then(result => callback(null, result)).catch(error => callback(error, null))
+    }
+
+    return response
+  }
+
+  /**
+	 * This method makes a private API request.
+	 * @param  {String}   method   The API method (public or private)
+	 * @param  {Object}   params   Arguments to pass to the api call
+	 * @param  {Function} callback A callback function to be executed when the request is complete
+	 * @return {Object}            The request object
+	 */
+  privateMethod(method:string, params:any, callback:Function) {
+    params = params || {}
+
+    // Default params to empty object
+    if (typeof params === 'function') {
+      callback = params
+      params = {}
+    }
+
+    const path = '/' + this.config.version + '/private/' + method
+    const url = this.config.url + path
+
+    if (!params.nonce) {
+      params.nonce = +new Date() * 1000 // spoof microsecond
+    }
+
+    if (this.config.otp !== undefined) {
+      params.otp = this.config.otp
+    }
+
+    const signature = getMessageSignature(path, params, this.config.secret, params.nonce)
+
+    const headers = {
+      'API-Key': this.config.key,
+      'API-Sign': signature
+    }
+
+    const response = rawRequest(url, headers, params, this.config.timeout)
+
+    if (typeof callback === 'function') {
+      response.then(result => callback(null, result)).catch(error => callback(error, null))
+    }
+
+    return response
+  }
+}
+
+export { KrakenClient }
